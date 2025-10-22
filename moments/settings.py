893dad3ed6c50d3ddc77ca6830c01a6a312c6df6
Jaideep <vinnijaideep@gmail.com>
2025-10-23 00:30:17 +0530

    corrections

--- moments/settings.py
+++ moments/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moments.pythonanywhere.com','127.0.0.1']
 
 # Application definition
 
